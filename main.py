43ee5f0920be98ab4cb46896994874008d1833b3
dilisa25 <dward@brooklynsteamcenter.org>
2026-02-11 10:17:34 -0500

    change (#13) (#16) #13
    Co-authored-by: Shaniya M <smack@brooklynsteamcenter.org>

--- main.py
+++ main.py
@@ -57,7 +57,85 @@ def load_user(user_id):
 def index():
     return render_template("homepage.html.jinja")
 
+@app.route("/map")
+def donate():
+     return render_template("map.html.jinja")
+
 
 @app.route("/donate")
 def donate():
      return render_template("donate.html.jinja")
+
+
+@app.route("/report")
+def index():
+    return render_template("report.html.jinja")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        connection = connect_db()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM `User` WHERE `Email` = %s AND `Password` = %s", (email, password) )
+        result = cursor.fetchone()
+        connection.close()
+
+        if result is None:
+            flash("Invalid email or password")
+            return redirect(url_for("login"))
+        
+        user = User(result)
+        login_user(user)
+
+        return redirect(url_for("index"))
+
+    return render_template("login.html.jinja")
+
+@app.route("/logout", methods=['GET', 'POST'] )
+@login_required
+def logout():
+    logout_user() # Logs out the current user
+    flash("You have been logged out.") # Notify the user
+    return redirect("/")
+
+@app.route('/signup', methods=["POST", "GET"])# User Registration
+def signup():
+    # Handle POST request for user registration
+    if request.method == "POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        password_repeat=request.form["repeat_password"]
+        address=request.form["address"]
+        birthdate=request.form["birthdate"]
+        # Validate password and confirmation
+        if password != password_repeat:
+            flash("Passwords do not match")
+            # Redirect back to the signup page
+        elif len(password) < 8:
+         flash("Password must be at least 8 characters long") 
+         # Redirect back to the signup page    
+        else:
+            connection = connect_db()
+            
+            cursor = connection.cursor()
+            # Insert new user into the database
+            try:
+                cursor.execute("""
+                    INSERT INTO `User` (`Name`, `Password`, `Email`, `Address`)
+                    VALUES (%s, %s, %s, %s)
+                """, (name, password, email, address))
+                connection.close()
+            # Handle duplicate email error
+            except pymysql.err.IntegrityError:
+                flash("User with that email already exists")
+                connection.close()
+            # If registration is successful, redirect to login page
+            else:
+                return redirect('/login')
+        
+    return render_template("signup.html.jinja")
